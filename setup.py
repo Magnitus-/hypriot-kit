2b3fc1722b5392d128b4ba211e1fe45c39ce579d
Eric Vallee <eric_vallee@webificservices.com>
2018-03-13 01:54:53 -0400

    Added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='hypriotkit',
+      version='0.1.0',
+      description='Hypriot OS Building Orchestration Tool',
+      author='Eric Vallee',
+      author_email='eric_vallee@webificservices.com',
+      url='https://github.com/Magnitus-/hypriot-kit',
+      licence='MIT License',
+      packages=['hypriotkit'],
+      platforms='Linux',
+      scripts=['hypriotkit'])
